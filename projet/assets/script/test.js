ca24ca6e601b434ccfb28b72498a1979ee1820be
baboux <roquinbaptiste@gmail.com>
2023-05-14 17:41:37 +0200

    scroll de la page de presentation fait

--- projet/assets/script/test.js
+++ projet/assets/script/test.js
@@ -1,14 +1,6 @@
-
-function essai() {
-    let bulles = document.getElementsByClassName("bouton");
-    console.log(bulles);
-    for (let item of bulles) {
-        console.log(item);
+function scrollPageBelowPresentation(){
+        window.scroll({
+            top: window.innerHeight,
+            behavior: "smooth",
+        });
     }
-
-    for (var i = 0; i < bulles.length; i++) {
-        console.log(bulles); //second console output
-    }
-}
-
-essai();
