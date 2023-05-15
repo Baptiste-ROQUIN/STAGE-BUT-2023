bb7021aa6be0082dfae9b621a220459e14f8aed0
baboux <roquinbaptiste@gmail.com>
2023-05-15 12:04:05 +0200

    Le menu commence a avoir une bonne tete, mais le code vraiment pas / ce qui rend les prochaines implementation complexe

--- projet/assets/script/test.js
+++ projet/assets/script/test.js
@@ -1,6 +1,6 @@
 function scrollPageBelowPresentation(){
         window.scroll({
-            top: window.innerHeight,
+            top: (window.innerHeight*0.93),
             behavior: "smooth",
         });
     }
