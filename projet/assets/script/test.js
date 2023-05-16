44a118493cd02475192af76eed5b44c7fe1a8b1c
ROQUIN <113183790+Baptiste-ROQUIN@users.noreply.github.com>
2023-05-16 21:26:28 +0200

    menu_fait_en_mieux

--- projet/assets/script/test.js
+++ projet/assets/script/test.js
@@ -3,4 +3,26 @@ function scrollPageBelowPresentation(){
             top: (window.innerHeight*0.93),
             behavior: "smooth",
         });
-    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('HTML prêt !');
+   
+    
+    console.log(liens_menu)
+
+
+    
+    
+});
+
+window.onscroll = function(){
+    const header = document.getElementById("header_changeant");
+    const liens_menu = document.querySelector(".menu>li>a");
+    if (scrollY > 100)
+        header.style.backgroundColor = "black";
+        liens_menu.style.color = "white";
+    if ((scrollY <= 100))
+        header.style.backgroundColor = "transparent";
+        liens_menu.style.color = "black"
+}
