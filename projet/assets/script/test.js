321f5c2b26b341e9f66f1c15fac6e453dd4cc6dc
ROQUIN <113183790+Baptiste-ROQUIN@users.noreply.github.com>
2023-05-17 17:22:57 +0200

    bouton retour haut de page + debut mini menu a gauche

--- projet/assets/script/test.js
+++ projet/assets/script/test.js
@@ -5,18 +5,38 @@ function scrollPageBelowPresentation(){
         });
 }
 
+function afficher() {
+    const text_a_afficher = document.querySelector("#info_CONSTANTINE>p");
+    console.log(text_a_afficher);
+    if (text_a_afficher.style.display == "none")
+        text_a_afficher.style.display = "block";
+    else text_a_afficher.style.display = "none";
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('HTML prêt !');
-   
+   const info_constantine = document.getElementById("info_CONSTANTINE");
+   info_constantine.addEventListener("click", afficher)
+
+   const btnScrollTop = document.getElementById("scrollUp");
+   btnScrollTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
 
 });
+
+
 
 window.onscroll = function(){
     const header = document.getElementById("header_changeant");
     const li_menu = document.querySelectorAll(".menu>li");
     const liens_menu = document.querySelectorAll(".menu>li>a");
     const image = document.getElementById("logo_ANRH")
-    console.log(image)
     
     if (scrollY > 100) {
         header.style.backgroundColor = "black";
@@ -40,4 +60,11 @@ window.onscroll = function(){
         image.style.visibility = "hidden";
     }
 
+    // afficher ou non le bouton pour remonter dans la page
+    const btnScrollTop = document.getElementById('scrollUp');
+    if (scrollY > innerHeight) {  // Quand on est à 200pixels du haut de page,
+        btnScrollTop.style.right = "10px"; // Replace à 10pixels de la droite l'image
+    } else { 
+        btnScrollTop.style.right = "-100px"; // Enlève les attributs CSS affectés par javascript
+    }
 }
