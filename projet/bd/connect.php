91d167c22b68e9f7bca59a28976545a0e0762614
ROQUIN <113183790+Baptiste-ROQUIN@users.noreply.github.com>
2023-06-05 17:06:27 +0200

    connexion avec xampp + changement ds carto

--- projet/bd/connect.php
+++ projet/bd/connect.php
@@ -1,6 +1,13 @@
 <?php
+    /*
     define('USER',"baboux");
     define('PASSWD',"baboux");
     define('SERVER',"localhost");
     define('BASE',"DBroquin");
+    */
+
+    define('USER',"root");
+    define('PASSWD',"root");
+    define('SERVER',"localhost");
+    define('BASE',"stage_but2");
 ?>
